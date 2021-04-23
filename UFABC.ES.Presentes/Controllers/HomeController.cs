3b8c6a7ea9823557f305e2ca1d34cce2efe9df7a
Luiz Fernando de Sousa Santos <50185930+lfernandoss@users.noreply.github.com>
2021-04-23 18:21:01 -0300

    alteraçoes finais

--- UFABC.ES.Presentes/Controllers/HomeController.cs
+++ UFABC.ES.Presentes/Controllers/HomeController.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using RestSharp;
 using UFABC.ES.Presentes.Models;
+using Newtonsoft.Json;
 
 namespace UFABC.ES.Presentes.Controllers
 {
@@ -22,6 +24,46 @@ namespace UFABC.ES.Presentes.Controllers
         {
             return View();
         }
+        public class CadastroDTO
+        {
+            public int id { get; set; }
+            public string nomePresenteador { get; set; }
+            public string nomePresenteada { get; set; }
+            public string tipoPresenteado { get; set; }
+            public string relacao { get; set; }
+            public string tipoDataComemorativa { get; set; }
+            public  string personalidade1 { get; set; }
+            public  string personalidade2 { get; set; }
+        }
+
+        public async Task<IActionResult> enviarSolicitacaoAsync(CadastroDTO cadastro)
+        {
+            var client = new RestClient("http://localhost:8080");
+            var request = new RestRequest("api/cadastro", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new { nomePresenteador = $@"{cadastro.nomePresenteador}", nomePresenteada = $@"{cadastro.nomePresenteada}",
+                tipoPresenteado = $@"{cadastro.tipoPresenteado}",
+                relacao = $@"{cadastro.relacao}",
+                tipoDataComemorativa = $@"{cadastro.tipoDataComemorativa}",
+                personalidade1 = $@"{cadastro.personalidade1}",
+                personalidade2 = $@"{cadastro.personalidade2}"
+            });
+            client.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            var resposta = await client.ExecuteAsync(request);
+            CadastroDTO deserializedProduct = JsonConvert.DeserializeObject<CadastroDTO>(resposta.Content);
+
+            return Json(deserializedProduct.id);
+        }
+        public async Task<IActionResult> buscarSolicitacaoAsync(int idsolicitacao)
+        {
+            var client = new RestClient("http://localhost:8080");
+            var request = new RestRequest($@"/api/sugestao/{idsolicitacao}", Method.GET);
+            
+            client.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            var resposta = await client.ExecuteAsync(request);
+
+            return Json(resposta.Content);
+        }
 
         public IActionResult Privacy()
         {
@@ -33,5 +75,7 @@ namespace UFABC.ES.Presentes.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
--- UFABC.ES.Presentes/wwwroot/js/script.js
+++ UFABC.ES.Presentes/wwwroot/js/script.js
@@ -27,3 +27,77 @@ function diaDosNamorados(){
     document.getElementById("corpo").style.backgroundImage = 'url(../imagens/diaDosNamorados.jpg)';
 }
 
+function enviarSolicitacao() {
+    $("#myModal").removeClass("in");
+    $(".modal-backdrop").remove();
+    $("#myModal").hide();
+    
+
+
+    $.ajax({
+        url: "http://localhost:5001/Home/enviarSolicitacao",
+        type: 'POST',
+        data: {
+            nomePresenteador: `${document.getElementById('validationCustom01').value}`,
+            nomePresenteada: `${document.getElementById('validationCustom02').value}`,
+            tipoPresenteado: `${$("#validationCustom04 option:selected").text()}`,
+            relacao: `${$("#validationCustom05 option:selected").text()}`,
+            tipoDataComemorativa: `${$("#validationCustom06 option:selected").text()}`,
+            personalidade1: `${$("#validationCustom08 option:selected").text()}`,
+            personalidade2: `${$("#validationCustom09 option:selected").text()}`
+        },
+        success: function (result) {
+            console.log("teste");
+
+            $.ajax({
+                contentType: 'application/json',
+                cache: false,
+                method: 'GET', 
+                url: "http://localhost:5001/Home/BuscarSolicitacao?idsolicitacao=" + result,
+                success: function (result) {
+                    console.log(result);
+                   
+                }
+            });
+        }
+    });
+
+    document.getElementById("listaPresentes").innerHTML = `<table class="table table-dark table-hover">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Nome do Presente</th>
+                      <th scope="col">Descrição</th>
+                      <th scope="col">Site para compra</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Marvel</td>
+                      <td>Livro de fantasia</td>
+                      <td ><a href="https://www.amazon.com.br/Trilogia-Star-Wars-IV-VI/dp/B07GCQGHNS">Link aqui!</a></td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>The Marvel Book</td>
+                      <td>Livro de fantasia</td>
+                      <td ><a href="https://www.amazon.com.br/Marvel-Book-Expand-Knowledge-Universe/dp/146547899X/ref=asc_df_146547899X/?tag=googleshopp00-20&linkCode=df0&hvadid=379736262876&hvpos=&hvnetw=g&hvrand=3509759156428997863&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001776&hvtargid=pla-820491794135&psc=1">Link aqui!</a></td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td>Batman: Criaturas da Noite</td>
+                      <td>Livro de fantasia</td>
+                      <td ><a href="https://www.amazon.com.br/Batman-Criaturas-Noite-Lendas-Livro-ebook/dp/B078WCBPLM/ref=sr_1_4?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=livro+dc&qid=1619030343&s=books&sr=1-4">Link aqui!</a></td>
+                    </tr>
+                    
+                  </tbody>
+                </table>`
+
+    //$.ajax({
+    //    url: "demo_test.txt", success: function (result) {
+    //        $("#div1").html(result);
+    //    }
+    //});
+
+}
